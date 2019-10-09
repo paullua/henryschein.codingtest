f047d3962f27f5044f94b61f53f11a9ae96c0ed8
Paul Land <paul.land1993@gmail.com>
2019-10-08 22:23:53 -0600

    First working version

--- src/main/java/com/paulland/henryschein/codingtest/csvController.java
+++ src/main/java/com/paulland/henryschein/codingtest/csvController.java
@@ -16,7 +16,64 @@ public class csvController {
   public String formatCsv(@PathVariable("stringCsv") String csv) {
     String formattedString = "";
 
+    while(!csv.isEmpty()) {
+      // Handles values enclosed in quotes
+      if (csv.charAt(0) == '"') {
+        String[] temp = substringBetweenQuotes(csv);
+        formattedString += "[" + temp[0] + "]";
+        csv = temp[1];
+      }
+
+      // Handles new-line and carriage-return
+      else if (csv.charAt(0) =='\r' || csv.charAt(0) == '\n') {
+        while (!csv.isEmpty() && (csv.charAt(0) == '\r' || csv.charAt(0) == '\n')) {
+          formattedString += csv.charAt(0);
+          csv = csv.substring(1);
+        }
+      }
+
+      // Handles values not enclosed with quotes
+      else {
+        formattedString += "[";
+
+        while (csv.charAt(0) != ',') {
+          formattedString += csv.charAt(0);
+          csv = csv.substring(1);
+        }
+
+        formattedString += "]";
+      }
+
+      // Replace the commas after values with spaces
+      if (!csv.isEmpty() && csv.charAt(0) == ',') {
+        formattedString += " ";
+        csv = csv.substring(1);
+      }
+    }
+
     return formattedString;
+  }
+
+  private String[] substringBetweenQuotes(String string) {
+
+    String returnString = "";
+    int i = 0;
+
+    // Move past the first quote
+    if (string.charAt(0) == '"') {
+      string = string.substring(1);
+    }
+
+    while (i + 1 <= string.length() && !string.substring(i, i + 1).equals("\"")) {
+      returnString += string.charAt(i);
+      i++;
+    }
+
+    if (i + 1 <= string.length()) {
+      return new String[]{returnString, string.substring(i + 1)};
+    }
+
+    return new String[]{returnString, string.substring(i)};
   }
 
 }
